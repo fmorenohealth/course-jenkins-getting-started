bb472e644ad567b2df96b5610983bfd105ecb712
fmorenohealth <fmoreno@bamboohealth.com>
2023-02-28 09:18:48 -0500

    x1
    x1

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,10 +4,21 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/fmorenohealth/jgsu-spring-petclinic.git', branch: 'main'
+                //git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
-        stage('Build') {
+        stage('Compile') {
+            steps {
+            sh './mvnw clean compile'
+            }
+        }
+        stage('Test') {
+            steps {
+           sh './mvnw test'
+            }
+        }
+        stage('Package') {
             steps {
                 sh './mvnw clean package'
                 //sh 'false'    // true   // add comment2
@@ -17,8 +28,8 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
+                //}
+                //changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
